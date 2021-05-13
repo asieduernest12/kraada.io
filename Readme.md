4d7cabdf017559a06586386a3aa1b1a78dfaecd8
Ernest <asieduernest@yahoo.co.uk>
2021-05-13 10:24:54 -0400

    locked and loaded

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Find your Ghanaian names
+Names are very important to Ghanaians
+You can make friends with someone by just being born on the same day and nothing else
+Find your name Ghanaian name and find your kin.
---
+++ js/info.js
@@ -0,0 +1,100 @@
+ [{
+    "sunday" :{
+        "male": {
+            "names": ["kwesi","akwesi"],
+            "song_of_praise": "",
+            "question": ""
+        },
+        
+        "female": {
+            "names": ["Esi"],
+            "song_of_praise": "",
+            "question": ""
+        }
+        
+    },
+    "monday" :{
+        "male": {
+            "names": ["kwado", "kwadwovi"],
+            "song_of_praise": "",
+            "question": ""
+        },
+        
+        "female": {
+            "names": ["adwoa", "dwodaba", "adwo", "adwovi"],
+            "song_of_praise": "",
+            "question": ""
+        }
+        
+    },
+    "tuesday" :{
+        "male": {
+            "names": ["kwabena","koby"],
+            "song_of_praise": "",
+            "question": ""
+        },
+        
+        "female": {
+            "names": ["abena"],
+            "song_of_praise": "",
+            "question": ""
+        }
+        
+    },
+    "wednesday" :{
+        "male": {
+            "names": ["kweku"],
+            "song_of_praise": "",
+            "question": ""
+        },
+        
+        "female": {
+            "names": ["eku", "ekusika", "ekushika", "akushika"],
+            "song_of_praise": "",
+            "question": ""
+        }
+        
+    },
+    "thursday" :{
+        "male": {
+            "names": ["yaw"],
+            "song_of_praise": "",
+            "question": ""
+        },
+        
+        "female": {
+            "names": ["yaa"],
+            "song_of_praise": "",
+            "question": ""
+        }
+        
+    },
+    "friday" :{
+        "male": {
+            "names": ["kofi","fiifi"],
+            "song_of_praise": "",
+            "question": ""
+        },
+        
+        "female": {
+            "names": ["afua", "afi", "efua", "afiba", "efe"],
+            "song_of_praise": "",
+            "question": ""
+        }
+        
+    },
+    "saturday" :{
+        "male": {
+            "names": ["kwame","kwaamina"],
+            "song_of_praise": "",
+            "question": ""
+        },
+        
+        "female": {
+            "names": ["ama"],
+            "song_of_praise": "",
+            "question": ""
+        }
+        
+    }
+}]
